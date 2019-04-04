8a1db705669013c6c749a98a75475760d9a7324f
Andrew Sanchez <ans046@ucsd.edu>
2019-04-04 01:22:30 -0700

    encoder algorithm complete

--- program1.c
+++ program1.c
@@ -1,3 +1,10 @@
+/*
+ * CSE141L Custom Architecture Emulator
+ *
+ * Description: 15 bit FEC encoder/decoder
+ * Authors: Andrew Sanchez & Matthew Taylor
+ * Date: 4/1/2019
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -11,6 +18,8 @@ u_byte mem [MEM_SIZE];
 
 void fillMem( void );
 void printMem( void );
+void fec_encode( void );
+u_byte xor_bits( u_byte );
 
 int main( void ) {
   //use current time as seed for random generator
@@ -18,6 +27,7 @@ int main( void ) {
 
   fillMem();
   printMem();
+  fec_encode();
   return 0;
 }
 
@@ -43,27 +53,37 @@ void printMem( void ) {
 }
 
 /*Forward Error Code (encoding algorithm)*/
-void fec_encode() {
+void fec_encode( void ) {
   //original memory
   u_byte mem1 = 0x07;
-  u_byte mem0 = 0xAA;
-
-  //masks used for bit manipulation
-  u_byte mask1 = 0x0F;  // 00001111
+  u_byte mem0 = 0xEE;
 
   //encoded memory
   u_byte new_mem1 = 0x00;
   u_byte new_mem0 = 0x00;
 
-  //parity bits
-  parity_bit8_mask = 0x80;
-  parity_bit
-
   //fec encoding algorithm
-  new_mem1 = (mem1 & mask1) << 4;
-  //create parity bit 8 here
-  new_mem1 = (mem0 >> 4) | new_mem1;
+  new_mem1 = (mem1 << 4) | (mem0 >> 4);
+
+  //parity bits
+  u_byte par_bit8 = xor_bits( new_mem1 );
+  u_byte par_bit4 = xor_bits( (mem0 << 3) | (mem0 >> 4) | (mem1) );
+  u_byte par_bit2 = xor_bits( (mem0 << 4) | ((mem0 >> 4) >> 1) | (mem1 << 1));
+  u_byte par_bit1 = xor_bits( (mem0 >> 4) | ( mem0 << 4) | (mem1 << 1) );
+
+  new_mem0 = ( (~par_bit8) + 0x01) | ((mem0 & 0x0E) << 3) |
+             (par_bit4 << 4) | ((mem0 & 0x01) << 3) |
+             (par_bit2 << 2) | (par_bit1);
 
   printf("Encoding Results:\n\nmem[1]= 0x%.2X \nmem[0]= 0x%.2X", new_mem1, new_mem0);
+}
 
+/*XOR all bits in byte NOTE: it's communinative and associative*/
+u_byte xor_bits( u_byte byte ) {
+  u_byte result = 0;
+  while (byte) {
+    result ^= byte & 1;
+    byte >>= 1;
+  }
+  return result;
 }
