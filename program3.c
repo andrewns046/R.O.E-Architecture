c4919deeb2e55992e1306a4e3f4b95e9a3e17ce9
Andrew Sanchez <ans046@ucsd.edu>
2019-04-12 10:48:57 -0700

    init program 3

---
+++ program3.c
@@ -0,0 +1,18 @@
+/*
+ * CSE141L Custom Architecture Emulator
+ *
+ * Description: Where's Waldo
+ * Authors: Andrew Sanchez & Matthew Taylor
+ * Date: 4/10/2019
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MEM_SIZE 64
+#define PAT_SIZE 4
+
+int main() {
+
+  return 0;
+}
