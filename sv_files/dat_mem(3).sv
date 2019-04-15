e0a7ff8ceaaa648f248247ef44d51b4afdee38c8
Andrew Sanchez <ans046@ucsd.edu>
2019-04-15 14:26:45 -0700

    changes DataAdr to MemAdr

--- sv_files/dat_mem(3).sv
+++ sv_files/dat_mem(3).sv
@@ -9,7 +9,7 @@
 // depth = 2**W (default value of AW = 8, may be changed)
 module dat_mem #(parameter AW=8)(
   input              CLK,                // clock
-  input    [AW-1:0]  DataAddress,		 // pointer
+  input    [AW-1:0]  MemAdr,		 // pointer
   input              ReadMem,			 // read enable	(may be tied high)
   input              WriteMem,			 // write enable
   input       [7:0]  DataIn,			 // data to store (write into memory)
@@ -18,23 +18,23 @@ module dat_mem #(parameter AW=8)(
   logic [7:0] core [2**AW]; 	   	     // create array of 2**AW elements (default = 256)
 
 // optional initialization of memory, e.g. seeding with constants
-//  initial 
+//  initial
 //    $readmemh("dataram_init.list", my_memory);
 
 // read from memory, e.g. on load instruction
   always_comb							 // reads are immediate/combinational
     if(ReadMem) begin
-      DataOut = core[DataAddress];
+      DataOut = core[MemAdr];
 // optional diagnostic print statement:
-//	  $display("Memory read M[%d] = %d",DataAddress,DataOut);
-    end else 
+//	  $display("Memory read M[%d] = %d",MemAdr,DataOut);
+    end else
       DataOut = 8'bz;			         // z denotes high-impedance/undriven
 
 // write to memory, e.g. on store instruction
   always_ff @ (posedge CLK)	             // writes are clocked / sequential
     if(WriteMem) begin
-      core[DataAddress] <= DataIn;
-//	  $display("Memory write M[%d] = %d",DataAddress,DataIn);
+      core[MemAdr] <= DataIn;
+//	  $display("Memory write M[%d] = %d",MemAdr,DataIn);
     end
 
 endmodule
