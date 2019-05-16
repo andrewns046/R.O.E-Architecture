30bf4abeda4808dd1a2134f6d08ea1e26cb4d89c
Andrew Sanchez <ans046@ucsd.edu>
2019-05-15 21:15:17 -0700

    final iteration of regdecoder ... probably

--- sv_files/regdecode.sv
+++ sv_files/regdecode.sv
@@ -2,18 +2,22 @@
 // Created by:         Andrew N. Sanchez
 // Design Name:        R.O.E arch
 // Module Name:        Reg Decode
+
 module regdecode ( input [2:0] set_pa,
-                   input [1:0] instr,
+                   input [1:0] lower_reg_addr,
                    input clk,
                    output logic [3:0] reg_addr);
-logic [1:0] pa;  //play area reg
+
+logic [1:0] pa;  // play area
+logic enable_write = set_pa[2];  // 3rd bit is enable
 
 always_ff @(posedge clk) begin
-  if(set_pa[2] == 1'b1) begin     //check first bit for enable signal
-    reg_addr <= {set_pa[1:0], instr};
-    pa <= set_pa[1:0];  // update play area
-  end else begin
-    reg_addr <= {pa, instr};  //pass through
-  end
+  if( enable_write == 1'b1 ) begin
+    reg_addr <= {set_pa[1:0], lower_reg_addr};
+    pa <= set_pa[1:0];
+  end else if( enable_write == 1'b0 )
+    reg_addr <= {pa, lower_reg_addr};
+  else
+    reg_addr <= 4'bXXXX;  //some garbage
 end
 endmodule
