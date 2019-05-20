0258eb72ed52c0321db4ee8c2d0712d6f68bf0c1
Andrew Sanchez <ans046@ucsd.edu>
2019-05-20 16:57:35 -0700

    updated pc to work with test bench

--- program_counter.sv
+++ program_counter.sv
@@ -3,18 +3,15 @@
 // Design Name:    R.O.E arch
 // Module Name:    program counter
 
-module program_counter #(parameter addr_w = 16)( input logic[addr_w-1:0] jump_here,
-                               input bnz, reset, halt, clk,
-                               output logic[addr_w-1:0] addr_out);
+module program_counter #(parameter addr_w = 16)( init, clk, bnz,
+                               input [addr_w-1:0] jump_here
+                               output logic[addr_w-1:0] addr_out = 0);
 
-  always_ff @(posedge clk)
-	if(reset)             // reset to 0 and hold there
-	  addr_out <= 'b0;
-	else if(halt)					// freeze
-	  addr_out <= addr_out;
-  else if(bnz)				// jump to definite address
-	   addr_out <= jump_here;
-	else							// normal advance thru program
-	  addr_out <= addr_out + 'b1;
-    
+always @(posedge clk) begin
+	if(!init) begin  //holds while init = 1
+    if(bnz)				    // jump to definite address
+	    addr_out <= jump_here;
+	  else							// normal advance thru program
+	    addr_out <= addr_out + 1;
+  end
 endmodule
