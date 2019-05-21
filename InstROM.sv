704a285c6a1ee10e30c81db28c2c09ab24276ad1
Andrew Sanchez <ans046@ucsd.edu>
2019-05-21 11:16:39 -0700

    fixed issues with instructions not being read properly

--- InstROM.sv
+++ InstROM.sv
@@ -2,7 +2,7 @@
 // Latest rev:     2017.10.26
 // Created by:     J Eldon
 // Design name:    CSE141L
-// Module Name:    InstROM 
+// Module Name:    InstROM
 
 // Generic instruction memory
 // same format as any lookup table
@@ -11,13 +11,13 @@
 module InstROM #(parameter IW=16, DW=9)(
   input       [IW-1:0] InstAddress,	// address pointer
   output logic[DW-1:0] InstOut);
-	 
+
   logic [DW-1:0] inst_rom [2**IW];	   // 2**IW elements, DW bits each
 // load machine code program into instruction ROM
-  initial 
-	$readmemb("instruct.txt", inst_rom);
+  initial
+	$readmemb("C:/Users/Tonik/Desktop/CSE141LEMU/instruct.txt", inst_rom);
 
-// continuous combinational read output  
+// continuous combinational read output
 //   change the pointer (from program counter) ==> change the output
   assign InstOut = inst_rom[InstAddress];
 
