e1b9117d74f4e8b793d4870c0a7879df8426abc7
Andrew Sanchez <ans046@ucsd.edu>
2019-05-22 00:50:21 -0700

    some test done + lut reads Mathew's array txt file

--- lut.sv
+++ lut.sv
@@ -1,14 +1,18 @@
+// Created By:     Andrew N. Sanchez
+// Create Date:    May 22, 2019
+// LUT Table: Stores line instructions
+
 module lut #(parameter addr_i_w = 8, addr_o_w = 16)(
   input logic[addr_i_w:0] lut_addr,
-  output logic[addr_o_w-1:0] lut_val);
+  output logic[addr_o_w-1:0] lut_out);
 
-// TODO: FILL this with addresses
-always_comb case(lut_addr)
-  3'h0:	 lut_val = 1'b0;
-  3'h1:	 lut_val = 1'b1;
-  3'h2:	 lut_val = 1'b0;
-  3'h3:	 lut_val = 1'b1;
-  default: lut_val = 1'b1;
-endcase
+  logic [addr_o_w:0] lut_table [2**addr_i_w]; // holds 256 values that are 16 wide
+
+  initial
+	$readmemb("C:/Users/Tonik/Desktop/CSE141LEMU/assembly/lut_program3.txt", lut_table);
+
+  // continuous combinational read output
+  // change the pointer (from program counter) ==> change the output
+  assign lut_out = lut_table[lut_addr];
 
 endmodule
